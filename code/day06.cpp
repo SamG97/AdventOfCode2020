f88d4c3db5a30f5a97435731185355af47df2c28
Sam Gooch <samg@myrtle.ai>
2020-12-06 18:06:47 +0000

    Faster day 6

--- code/day06.cpp
+++ code/day06.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <vector>
 
+static_assert(sizeof(unsigned int) >= 4);
+const char offset = 'a';
+
 std::vector<std::vector<std::string>> readInput() {
     std::vector<std::vector<std::string>> input;
     std::ifstream ifs{"inputs/day06.txt"};
@@ -24,38 +27,41 @@ std::vector<std::vector<std::string>> readInput() {
     return input;
 }
 
-int partOne(std::vector<std::vector<std::string>> groups) {
+int countBitsSet(unsigned int n) {
+    int count = 0;
+    while (n != 0) {
+        n &= n - 1;
+        ++count;
+    }
+    return count;
+}
+
+int partOne(const std::vector<std::vector<std::string>>& groups) {
     int total = 0;
     for (auto& group : groups) {
-        std::string result = "";
+        unsigned int result = 0;
         for (auto& passenger : group) {
-            std::string update;
-            std::sort(passenger.begin(), passenger.end());
-            std::set_union(result.begin(), result.end(), passenger.begin(),
-                           passenger.end(), std::back_inserter(update));
-            result = update;
+            for (auto& c : passenger) {
+                result |= 0b1 << (c - offset);
+            }
         }
-        total += result.size();
+        total += countBitsSet(result);
     }
     return total;
 }
 
-int partTwo(std::vector<std::vector<std::string>> groups) {
+int partTwo(const std::vector<std::vector<std::string>>& groups) {
     int total = 0;
     for (auto& group : groups) {
-        std::string result = "abcdefghijklmnopqrstuvwxyz";
+        unsigned int result = ~0;
         for (auto& passenger : group) {
-            std::string update;
-            std::sort(passenger.begin(), passenger.end());
-            std::set_intersection(result.begin(), result.end(),
-                                  passenger.begin(), passenger.end(),
-                                  std::back_inserter(update));
-            result = update;
-            if (result == "") {
-                break;
+            unsigned int passengerBits = 0;
+            for (auto& c : passenger) {
+                passengerBits |= 0b1 << (c - offset);
             }
+            result &= passengerBits;
         }
-        total += result.size();
+        total += countBitsSet(result);
     }
     return total;
 }
